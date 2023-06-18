218684a25185ca20b50818e7ddb139e29d3c8ab8
Bongo Juniorr <bongojuniorr99@gmail.com>
2023-06-18 10:49:53 +0700

    Dialogue Box

---
+++ Game/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    void Start() { }
+    void Update() { }
+
+    public class Message
+    {
+        public int actorID;
+        public string message;
+    }
+
+    public class Actor
+    {
+        public string name;
+        Unity.en
+    }
+
+}
